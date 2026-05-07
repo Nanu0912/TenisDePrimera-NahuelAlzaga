4141ee2f3877ecc32bb6f3a27010c7ee7df6b12a
Nahuel Alzaga <nahuelalzaga091218@gmail.com>
2026-05-07 17:48:38 -0300

    FrmLogin

--- TenisDePrimera/TDP.GUI499NA/Login.cs
+++ TenisDePrimera/TDP.GUI499NA/Login.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace TDP.GUI499NA
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
         }
