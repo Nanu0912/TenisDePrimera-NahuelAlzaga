76c5ac184db5019328ca4e145c4e85845637f44e
Nahuel Alzaga <nahuelalzaga091218@gmail.com>
2026-05-18 13:24:27 -0300

    Ajustes

--- TenisDePrimera/TDP.GUI499NA/Program.cs
+++ TenisDePrimera/TDP.GUI499NA/Program.cs
@@ -16,7 +16,7 @@ namespace TDP.GUI499NA
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Login());
         }
     }
 }
