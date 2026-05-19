0a5b98c5dfb5d011378aee956765963f2786ec3b
Nahuel Alzaga <nahuelalzaga091218@gmail.com>
2026-05-19 00:25:40 -0300

    be, dal cripto

---
+++ TenisDePrimera/TDP.BE499NA/UsuarioBE499NA.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDP.BE499NA
+{
+    internal class UsuarioBE499NA
+    {
+    }
+}
---
+++ TenisDePrimera/TDP.DAL499NA/UsuarioDAL499NA.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDP.DAL499NA
+{
+    internal class UsuarioDAL499NA
+    {
+    }
+}
